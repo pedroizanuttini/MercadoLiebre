7919f3899f848b8557ca8a5dcf053d4a03e9e6a7
pedroizanuttini <pedroizanuttini@gmail.com>
2022-12-21 00:10:14 -0300

    subo proyecto

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app= express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/registration', (req, res) => {
+    res.sendFile(__dirname + '/views/signup.html')
+});
+
