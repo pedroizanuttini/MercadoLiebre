057e3a6411755fc08191537f0a9c32cc8ca62713
pedroizanuttini <pedroizanuttini@gmail.com>
2023-05-08 11:31:20 +1000

    agrego primeros cambios a repositorio nuevo

--- src/routes/carrito.routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const{showCart}= require('../controllers/carrito.controllers');
-
-const router = Router();
-
-router.get('/', showCart) //http://localhost:3000/cart
-
-module.exports = router;
